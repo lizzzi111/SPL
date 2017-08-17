c5c684865431634b86fec80db7cdf4dfa7ecbc62
lizzzi111 <zinovyee@gmail.com>
2017-08-17 13:32:08 +0200

    corrected an error

--- SPL_Q6_results_analysis/visualizations.R
+++ SPL_Q6_results_analysis/visualizations.R
@@ -6,8 +6,7 @@ multiclass = read.csv2("./SPL_Q6_results_analysis/multiclass_fin.csv")
 result_plots = function(df, title = NULL) {
   
   list.of.packages = c("ggplot2")
-  new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[, 
-                                                                                "Package"])]
+  new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if (length(new.packages)) 
     install.packages(new.packages, dependencies = TRUE)
   library(ggplot2)
@@ -17,33 +16,31 @@ result_plots = function(df, title = NULL) {
   
   
   if (any(names(df) %in% "rmse")) {
-    accuracy = df$rmse
     title = ifelse(is.null(title), regr_title, title)
     ax_title_y = "Root Mean Squarred Error"
   } else {
-    accuracy = df$pcc
+    df$rmse = df$pcc
     title = ifelse(is.null(title), class_title, title)
     ax_title_y = "Percentage of Correctly Classified values"
   }
   
   
-  boxplot = ggplot(aes(y = accuracy, x = model), data = df) + 
+  boxplot = ggplot(aes(y = rmse, x = model), data = df) + 
             geom_boxplot() + 
             theme_bw() + 
             labs(y = ax_title_y) + 
             ggtitle(title)
   
-  line_plot = ggplot(df, aes(x = model, y = accuracy, colour = Input_type, 
-                             shape = Input_type, group = Input_type)) +
-    geom_line(size = 2) + 
-    geom_point(size = 4) + 
-    facet_grid(df$Class ~ .) + 
-    theme(text = element_text(size = 15), 
-          axis.text.x = element_text(angle = 90, hjust = 1), 
-          plot.title = element_text(size = 15,lineheight = 0.8, face = "bold")) + 
-    ggtitle(title) + 
-    theme_bw() + 
-    labs(y = ax_title_y)
+  line_plot = ggplot(data = df, aes(x = model, y = rmse, group = interaction(Input_type,Class), shape = Input_type)) +
+              geom_line(aes(linetype = Input_type, color = Input_type), size = 2) + 
+              geom_point(aes(color = Input_type), size = 4) + 
+              facet_grid(Class~.)+
+              theme(text = element_text(size = 15), 
+              axis.text.x = element_text(angle = 90, hjust = 1), 
+              plot.title = element_text(size = 15,lineheight = 0.8, face = "bold")) + 
+              ggtitle(title) + 
+              theme_bw() + 
+              labs(y = ax_title_y)
   
   output = list(boxplot = boxplot, lines = line_plot)
   return(output)
